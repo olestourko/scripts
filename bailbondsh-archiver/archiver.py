2ceca8ae29ac27c50fa132def60311201b174f2d
Oles Tourko <oles.tourko@digitalextremes.com>
2017-02-15 22:23:15 +0000

    Added audio-only archiving to script

--- bailbondsh-archiver/archiver.py
+++ bailbondsh-archiver/archiver.py
@@ -8,30 +8,27 @@ parser = argparse.ArgumentParser(description="Archives prank callsh from youtube
 parser.add_argument("file", help="A file with a lost of youtube channels.")
 parser.add_argument("path", help="The base path where videos will be archived.")
 parser.add_argument("-r", "--limit-rate", dest="limit_rate", help="Maximum download rate in bytes per second(e.g. 50K or 4.2M)")
-parser.add_argument("-a", "--audio", dest="get_audio", help="Also download a seperate audio file along with the video.", action="store_true")
+parser.add_argument("-a", "--audio-only", dest="audio_only", help="Archives only the audio.", action="store_true")
 args = parser.parse_args()
 
 if subprocess.call(["which", "youtube-dl"]) == 0:
 	f = open(args.file, 'r')
 	channels = json.loads(f.read())
 	base_path = args.path if args.path.endswith("/") else args.path + "/"
-
-        base_call = ["youtube-dl"]
-        if args.limit_rate:
-                base_call = base_call.extend(["--limit-rate", args.limit_rate])
-
+	base_call = ["youtube-dl"]
+	
+	if args.limit_rate:
+		base_call = base_call.extend(["--limit-rate", args.limit_rate])
+		
 	for key, url in channels.items():
-                call = list(base_call)
-                call.extend(["-o", base_path + '%(uploader)s/%(title)s-%(id)s', url])
-
-                #download videos
-		#subprocess.call(call)
-
-                #also download the audio files, if the flag is set
-                if args.get_audio:
-                    call = list(base_call)
-                    call.extend(["-o", base_path + '%(uploader)s/audio_only/%(title)s-%(id)s', url, "--extract-audio", "--audio-quality", "3"])
-                    subprocess.call(call)
+		if args.audio_only:
+			call = list(base_call)
+			call.extend(["-o", base_path + '%(uploader)s/%(title)s-%(id)s.%(ext)s', url, "--extract-audio", "--audio-quality", "3", "--audio-format", "m4a", "--embed-thumbnail"])
+			subprocess.call(call)
+		else:
+			call = list(base_call)
+			call.extend(["-o", base_path + '%(uploader)s/%(title)s-%(id)s', url])
+			subprocess.call(call)
 
 else:
 	print("youtube-dl not found.", file=sys.stderr)
