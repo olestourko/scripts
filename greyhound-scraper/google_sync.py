a6732548dd77d4d51d168fa18431207fa52d61bc
Oles <oles@olestourko.ca>
2017-02-20 10:16:55 -0500

    Writing junk to google sheets

--- greyhound-scraper/google_sync.py
+++ greyhound-scraper/google_sync.py
@@ -1,25 +1,23 @@
 from __future__ import print_function
 import httplib2
 import os
+import argparse
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-	import argparse
-	flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-	flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# available scopres are available here: https://developers.google.com/sheets/api/guides/authorizing#OAuth2Authorizing
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Greyhound Canada Scraper'
 
-def get_credentials():
+def get_credentials(flags):
 	"""Gets valid user credentials from storage.
 
 	If nothing has been stored, or if the stored credentials are invalid,
@@ -47,7 +45,25 @@ def get_credentials():
 	return credentials
 
 if __name__ == "__main__":
-	credentials = get_credentials()
+	parser = argparse.ArgumentParser(parents=[tools.argparser], description="Writes schedules to a google sheet.")
+	parser.add_argument("--sheet-id", "-s", dest="sheet_id", help="the google sheet ID", required=True)
+	args = parser.parse_args()
+
+	credentials = get_credentials(args)
 	http = credentials.authorize(httplib2.Http())
 	discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
-	service = discovery.build('sheets', 'v4', http=http,discoveryServiceUrl=discoveryUrl)
+	service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
+
+	#read from the spreedsheet
+	result = service.spreadsheets().values().get(spreadsheetId=args.sheet_id, range="A1").execute()
+	print(result)
+
+	#write to the spreedsheet
+	values = [
+		["BBB", "CCC"]
+	]
+	body = {
+		'values': values
+	}
+	result = service.spreadsheets().values().update(spreadsheetId=args.sheet_id, range="A1:B1", body=body, valueInputOption="RAW").execute()
+
